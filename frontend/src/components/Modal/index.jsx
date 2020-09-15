9cd20027c3400c28ee73ba531949d87f8cb244cd
PedroMendoncaBR <peedromendonca@gmail.com>
2020-09-15 11:09:22 -0300

    uploading frontend

---
+++ frontend/src/components/Modal/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Modal() {
+  return (
+    <>
+      <h1>OK</h1>
+    </>
+  );
+}
+
+export default Modal;
---
+++ frontend/src/routes/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Dashboard from '../pages/Dashboard';
+
+const Routes = () => {
+  return(
+    <Switch>
+      <Route path="/" exact component={Login} />
+      <Route path="/register" exact component={Register} />
+      <Route path="/dashboard" exact component={Dashboard} />
+    </Switch>
+  );
+}
+
+export default Routes;
