c63ccc0a31a6208010d85317275d2f77daebf504
JDanrley <you@example.com>
2020-09-16 22:55:42 -0300

    CORS configurado

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,10 +2,12 @@ import os
 from flask            import Flask
 import shapefile
 import postgresql
+from flask_cors import CORS
 
 #Getting the directory where the app is running
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+CORS(app)
 
 from app import views
