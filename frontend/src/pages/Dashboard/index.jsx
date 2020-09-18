bf873c01fd8ad82fb051b2f8dbcc7bb6256666fc
leonardomessias98 <messiasleonardo98@gmail.com>
2020-09-18 10:40:08 -0300

    preparing code to render new pages

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -1,35 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { Upload, message } from 'antd';
 
-import ModalConnection from './ModalConnection';
+import { DashboardContainer } from './styles';
 
-import { DashboardContainer, UploadIcon, SearchIcon } from './styles';
+import Acess from './components/Acess';
 
 import LogoHeader from '../../assets/images/Logo-white-bg.png';
 
-const { Dragger } = Upload;
-
-const props = {
-  name: 'shapefiles',
-  multiple: true,
-  action: 'http://localhost:5000/uploads',
-
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-};
-
 function Dashboard() {
-  const [openConnection, setOpenConnection] = useState(false);
   const history = useHistory();
 
   return (
@@ -44,42 +22,8 @@ function Dashboard() {
           />
         </header>
 
-        <ModalConnection 
-          open={openConnection} 
-          close={setOpenConnection} 
-        />
-
-        <div className="upload-search-container">
-
-          <div className="upload-container">
-            <Dragger className="dragger-props-custom" {...props}>
-              <div className="upload-dash">
-                <UploadIcon className="upload-icon" />
-
-                <span className="upload-desc">Clique ou arraste seus arquivos aqui</span>
-              </div>
-            </Dragger>
-
-            <button className="handle-button">Upload</button>
-          </div>
-
-          <div className="upload-container">
-            <button onClick={()=> setOpenConnection(true)} className="upload-dash search-dash">
-              <SearchIcon className="upload-icon" />
-
-              <span className="upload-desc search-desc">Pesquise no banco de dados</span>
-            </button>
-
-            <button 
-              onClick={()=> setOpenConnection(true)} 
-              className="handle-button"
-            >
-              Buscar
-            </button>
-          </div>
+        <Acess />
           
-        </div>
-
       </DashboardContainer>
     </>
   );
--- frontend/src/pages/Dashboard/styles.js
+++ frontend/src/pages/Dashboard/styles.js
@@ -3,25 +3,6 @@ import styled from 'styled-components';
 import Background from '../../assets/images/background-dashboard.png';
 
 
-import { RiUploadCloud2Line } from 'react-icons/ri';
-import { BsSearch } from 'react-icons/bs';
-
-
-
-export const UploadIcon = styled(RiUploadCloud2Line)`
-  font-size: 100px;
-  color: rgba(67, 219, 178, 0.25);
-
-  transition: ease-in-out 0.2s;
-`;
-
-export const SearchIcon = styled(BsSearch)`
-  font-size: 80px;
-  color: rgba(67, 219, 178, 0.25);
-
-  transition: ease-in-out 0.2s;
-`;
-
 export const DashboardContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -55,110 +36,6 @@ export const DashboardContainer = styled.div`
 
       &:hover {
         margin-left: 10px;
-      }
-    }
-  }
-
-  .upload-search-container {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-
-    width: 100%;
-    height: 100%;
-
-    padding-right: 250px;
-    padding-left: 250px;
-
-    .search-dash {
-      border: 4px solid rgba(67, 219, 178, 0.25);
-
-      &:hover {
-        border: 4px solid #33AC91 !important;
-      }
-      
-    }
-
-    .upload-container {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-
-      width: 350px;
-      height: max-content;
-
-      .dragger-props-custom {
-        border: none;
-        background: transparent;
-      }
-
-      .upload-dash {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        align-items: center;
-
-        width: 100%;
-        height: 250px;
-
-        padding-top: 35px;
-        padding-bottom: 35px;
-
-        background: #FFFFFF;
-        border: 4px solid rgba(67, 219, 178, 0.25);
-        border-radius: 50px;
-
-        cursor: pointer;
-        transition: ease-in-out 0.2s;
-
-        .search-desc {
-          margin-bottom: 20px;
-        }
-        
-        .upload-desc {
-          text-align: center;
-
-          font-size: 20px;
-          font-weight: 600;
-          color: rgba(67, 219, 178, 0.25);
-
-          transition: ease-in-out 0.2s;
-        }
-
-        &:hover {
-          border: 4px dashed #33AC91;
-
-          .upload-icon {
-            color: #33AC91;
-          }
-
-          .upload-desc {
-            color: #33AC91;
-          }
-        }
-      }
-
-      .handle-button {
-        width: 200px;
-        height: 45px;
-
-        margin-top: 50px;
-
-        color: #FFFFFF;
-        font-size: 20px;
-        font-weight: 600;
-
-        background-color: #43DBB2;
-        border: none;
-        border-radius: 50px;
-
-        transition: ease-in-out 0.2s;
-
-        &:hover {
-          background-color: #33AC91;
-        }
       }
     }
   }
