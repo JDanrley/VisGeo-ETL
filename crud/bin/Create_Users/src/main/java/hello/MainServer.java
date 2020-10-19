b7a00207ec527b0fe5118a33184a74dbd5e836e5
raquelribeiro2 <raqu3l.rodriguesribeiro@gmail.com>
2020-10-18 23:43:21 -0300

    Sprint 2

---
+++ crud/bin/Create_Users/src/main/java/hello/MainServer.java
@@ -0,0 +1,30 @@
+package hello;
+
+import static spark.Spark.*;
+
+
+public class MainServer {
+	
+	final static Model model = new Model();
+	
+    public static void main(String[] args) {
+
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 3333;
+        }
+        port(port);
+
+		staticFileLocation("/static");
+
+		Controller controller = new Controller(model);
+		
+		controller.login();
+		controller.register();
+		
+    }
+    
+}
