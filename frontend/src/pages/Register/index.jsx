c6628a896ace2e78082f2bd8af8408f14bd054b2
raquelribeiro2 <raqu3l.rodriguesribeiro@gmail.com>
2020-10-18 23:45:12 -0300

    Sprint 2

--- frontend/src/pages/Register/index.jsx
+++ frontend/src/pages/Register/index.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import { RegisterContainer, Lock, LoginIcon, RegisterIcon,
-  UserIcon } from './styles';
+import { RegisterContainer, Lock, LoginIcon, RegisterIcon, UserIcon } from './styles';
 
 import Logo from '../../assets/images/Logo.png';
 
+import apiUser from '../../services/apiUser';
+
 function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const history = useHistory();
+
+  async function handleRegister() {
+    try {
+      await apiUser.post('/register', { name, email, password });
+    } catch (err) {
+      alert('Cadastrado com sucesso.')
+      window.location.href = 'http://localhost:3000'
+    }
+  }
 
   return (
     <>
@@ -20,21 +33,21 @@ function Register() {
             <form>
               <div className="input-container">
                 <UserIcon className="user-icon" />
-                <input type="text" placeholder="Nome" />
+                <input type="text" onChange={(e) => setName(e.target.value)} placeholder="Nome" />
               </div>
 
               <div className="input-container">
                 <span className="email-symbol">@</span>
-                <input type="text" placeholder="E-mail" />
+                <input type="text" onChange={(e) => setEmail(e.target.value)} placeholder="E-mail" />
               </div>
 
               <div className="input-container">
                 <Lock className="password-icon" />
-                <input type="password" placeholder="Senha" />
+                <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Senha" />
               </div>
             </form>
 
-            <button className="log-in">
+            <button className="log-in" onClick={handleRegister}>
               <RegisterIcon />
               CADASTRAR
             </button>
